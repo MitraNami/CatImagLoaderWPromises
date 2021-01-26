4e9ac8cfb55e2c9dbc5f95d1da41bb51c35e2a71
Mitra Nami <a.mxnami@gmail.com>
2021-01-25 22:16:56 -0500

    Use promises

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
 
-loadImage('./images/cat1.jpg', (error, imgEl) => {
-  if (error) {
-    console.log(error.message);
-    return;
-  }
-  addImg(imgEl);
-  loadImage('./images/cat2.jpg', (error, imgEl) => {
-    if (error) {
-      console.log(error.message);
-      return;
-    }
-    addImg(imgEl);
-    loadImage('./images/cat3.jpg', (error, imgEl) => {
-      if (error) {
-        console.log(error.message);
-        return;
-      }
-      addImg(imgEl);
-    })
-  });
+// loadImage('./images/cat1.jpg', (error, imgEl) => {
+//   if (error) {
+//     console.log(error.message);
+//     return;
+//   }
+//   addImg(imgEl);
+//   loadImage('./images/cat2.jpg', (error, imgEl) => {
+//     if (error) {
+//       console.log(error.message);
+//       return;
+//     }
+//     addImg(imgEl);
+//     loadImage('./images/cat3.jpg', (error, imgEl) => {
+//       if (error) {
+//         console.log(error.message);
+//         return;
+//       }
+//       addImg(imgEl);
+//     })
+//   });
 
 
-});
-
-
+// });
 
 
 
+Promise.all([
+  loadImage('./images/cat1.jpg'),
+  loadImage('./images/cat2.jpg'),
+  loadImage('./images/cat3.jpg')
+])
+.then(imgElements => {
+  imgElements.forEach(imgElement => addImg(imgElement));
+})
+.catch(err => console.log(err.message));
   
--- helpers.js
+++ helpers.js
@@ -1,21 +1,43 @@
 
 // with callback
-const loadImage = function(url, callback) {
+// const loadImage = function(url, callback) {
 
-  const imgElement = document.createElement('img');
-  imgElement.src = url;
+//   const imgElement = document.createElement('img');
+//   imgElement.src = url;
 
-  imgElement.onload = function() {
-    callback(null, imgElement);
-  };
+//   imgElement.onload = function() {
+//     callback(null, imgElement);
+//   };
 
-  imgElement.onerror = function() {
-    const msg = `Could not load image at ${url}`;
-    callback(new Error(msg), null);
-  };
+//   imgElement.onerror = function() {
+//     const msg = `Could not load image at ${url}`;
+//     callback(new Error(msg), null);
+//   };
   
-};
+// };
 
+
+
+// with promise
+const loadImage = function(url) {
+
+  return new Promise((resolve, reject) => {
+
+    const imgEl = document.createElement('img');
+    imgEl.src = url;
+
+    imgEl.onload = function() {
+      resolve(imgEl);
+    };
+
+    imgEl.onerror = function() {
+      const msg = `Could not load image at ${url}`;
+      reject(new Error(msg));
+    };
+
+  });
+
+};
 
 
 
